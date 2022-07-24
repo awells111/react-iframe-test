00bfed886e534e3d52aaf1e7d3b7b9de59c8d0a7
awells111 <awells111@users.noreply.github.com>
2022-07-24 17:19:33 -0400

    Added app labels

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* Show iframe if the IFRAME_URL isn't undefined */}
-        {process.env.REACT_APP_IFRAME_URL && <Iframe IFRAME_URL={process.env.REACT_APP_IFRAME_URL}/>}
+        {process.env.REACT_APP_LABEL && <h2>{process.env.REACT_APP_LABEL}</h2>}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        {/* Show iframe if the IFRAME_URL isn't undefined */}
+        {process.env.REACT_APP_IFRAME_URL && <Iframe IFRAME_URL={process.env.REACT_APP_IFRAME_URL}/>}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// todo if REACT_APP_USE_PARENT_HOOK, take the devtools hook from the parent
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
